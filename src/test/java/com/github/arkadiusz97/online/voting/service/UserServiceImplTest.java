ef628502d79b615727dee5f67e202bd32697b58a
arkadiusz97 <arkadiusz_bok@o2.pl>
2024-04-11 22:52:27 +0200

    add test

--- src/test/java/com/github/arkadiusz97/online/voting/service/UserServiceImplTest.java
+++ src/test/java/com/github/arkadiusz97/online/voting/service/UserServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.github.arkadiusz97.online.voting.service;
 
+import com.github.arkadiusz97.online.voting.domain.Role;
 import com.github.arkadiusz97.online.voting.domain.User;
 import com.github.arkadiusz97.online.voting.dto.responsebody.UserDTO;
 import com.github.arkadiusz97.online.voting.repository.RoleRepository;
@@ -19,7 +20,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Arrays;
-import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -27,6 +27,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceImplTest {
+
     @Autowired
     private UserServiceImpl userServiceImpl;
 
@@ -50,22 +51,27 @@ public class UserServiceImplTest {
 
     @BeforeEach
     public void setup() {
-        LinkedList<User> usersToSave = SampleDomains.getSampleUsers();
-        usersToSave.forEach(user -> {
-            userRepository.save(user);
-        });
-        PageRequest pageRequest = PageRequest.of(1, 2);
-        List<User> usersFromPage = Arrays.asList(usersToSave.get(3), usersToSave.get(4));
-        Page<User> userPage = new PageImpl<>(usersFromPage, pageRequest, 2);
-        Mockito.when(userRepository.findAll(pageRequest)).thenReturn(userPage);
         userServiceImpl = new UserServiceImpl(userRepository, userRoleRepository, roleRepository, mailService,
             passwordEncoder, passwordGeneratorService);
     }
 
     @Test
     public void whenShowMany_thenGetUsers() {
-        List<UserDTO> users = userServiceImpl.showMany(1, 2);
-        assertThat("some-mail4@domain.eu").isEqualTo(users.get(0).email());
-        assertThat(2).isEqualTo(users.size());
+        LinkedList<User> users = SampleDomains.getSampleUsers();
+        PageRequest pageRequest = PageRequest.of(1, 2);
+        List<User> usersFromPage = Arrays.asList(users.get(3), users.get(4));
+        Page<User> userPage = new PageImpl<>(usersFromPage, pageRequest, 2);
+        Mockito.when(userRepository.findAll(pageRequest)).thenReturn(userPage);
+        List<UserDTO> result = userServiceImpl.showMany(1, 2);
+        assertThat("some-mail4@domain.eu").isEqualTo(result.get(0).email());
+        assertThat(2).isEqualTo(result.size());
+    }
+
+    @Test
+    public void whenRegisterNewUser_thenGetThisUser() {
+        Mockito.when(roleRepository.findByName("ROLE_USER")).thenReturn(new Role("ROLE_USER"));
+        String userMail = "some-user@domain.com";
+        String message = userServiceImpl.registerNewUser(userMail);
+        assertThat(message).isEqualTo("Registered a new user with " + userMail + " email");
     }
 }
